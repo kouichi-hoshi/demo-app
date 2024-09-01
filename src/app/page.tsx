7d902daf678b4d94ee8983662bbe574377356f0e
kouichi.hoshi <kouichi.hoshi@me.com>
2024-09-01 22:19:55 +0900

    tailwindのlint設定を追加（class名のソートとcssModulesに記載されたclass名をlintから除外する）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className='p-4'>
       <h1 className='mb-4'>demo app</h1>
       <button className='button'>demo button</button>
-      <button className='bg-blue-600 text-xs font-medium inline-block px-6 py-2.5  leading-tight rounded'></button>
     </main>
   )
 }
