a2a7af237adb881980b49994619ae6e8f55a3fda
kouichi.hoshi <kouichi.hoshi@me.com>
2024-09-01 18:48:05 +0900

    各npmパッケージ追加：TOPページテスト用ボタン追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>demo app</h1>
+    <main className="p-4">
+      <h1 className="mb-4">demo app</h1>
+      <button className="button">demo button</button>
     </main>
   );
 }
