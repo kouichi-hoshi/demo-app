e42291eeaf91651a58bc66de40a90218b710267a
kouichi.hoshi <kouichi.hoshi@me.com>
2024-09-03 22:44:59 +0900

    meta siteUrlを更新

--- src/app/_constants/constants.ts
+++ src/app/_constants/constants.ts
@@ -1,5 +1,5 @@
 export const SITE_META = {
   siteTitle: 'Demo app - Stella Design',
-  siteUrl: 'localhost:3000',
+  siteUrl: 'https://demo-app.stella-design.biz',
   siteDesc: 'Demo app for Stella Design',
 }
