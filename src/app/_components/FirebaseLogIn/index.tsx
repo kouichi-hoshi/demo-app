74364def84439f9de52919c2a6d8e2780c6be7ca
kouichi.hoshi <kouichi.hoshi@me.com>
2024-09-12 18:20:40 +0900

    アクセストークンの出力を削除、console.logを整理

--- src/app/_components/FirebaseLogIn/index.tsx
+++ src/app/_components/FirebaseLogIn/index.tsx
@@ -24,26 +24,18 @@ export default function FirebaseLogIn() {
 
     signInWithPopup(auth, provider)
       .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result)
         if (credential) {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          const token = credential.accessToken
-          // The signed-in user info.
+          //TODO:不要なコードを後で削除する
+          //const token = credential.accessToken
           const user = result.user
-          // IdP data available using getAdditionalUserInfo(result)
-          console.log(`Signed in as ${user.displayName} with token: ${token}`)
-        } else {
-          console.log('No credentials returned from the provider.')
+          console.log('ログインしました', user.displayName)
         }
       })
       .catch((error) => {
-        // Handle Errors here.
         const errorCode = error.code
         const errorMessage = error.message
-        // The email of the user's account used.
         const email = error.customData.email
-        // The AuthCredential type that was used.
         const credential = GoogleAuthProvider.credentialFromError(error)
 
         //TODO: エラー処理を追加する
