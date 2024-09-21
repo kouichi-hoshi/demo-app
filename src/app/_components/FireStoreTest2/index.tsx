660c9df1ef934f3c5cd7c7772bec90f96f48b10c
kouichi.hoshi <kouichi.hoshi@me.com>
2024-09-21 11:37:57 +0900

    タスクを追加するコードを実装

--- src/app/_components/FireStoreTest2/index.tsx
+++ src/app/_components/FireStoreTest2/index.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { db } from '@/_lib/firebase'
-import { collection, onSnapshot } from 'firebase/firestore'
+import { collection, addDoc, onSnapshot } from 'firebase/firestore'
 import { useAuth } from '@/_provider/FirebaseProvider'
 
 type TaskProps = {
@@ -15,6 +15,7 @@ type TaskProps = {
 export default function FireStoreTest2() {
   const [tasks, setTasks] = useState<TaskProps[]>([])
   const currentUser = useAuth()
+  const inputRef = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
     if (!currentUser || !currentUser.uid) {
@@ -38,6 +39,34 @@ export default function FireStoreTest2() {
     return () => unsubscribe()
   }, [currentUser])
 
+  const handleAddTask = async () => {
+    if (!inputRef.current) {
+      return
+    }
+
+    const taskTitle = inputRef.current.value
+
+    if (taskTitle.trim() === '') {
+      return
+    }
+
+    // currentUser または currentUser.uid が null の場合は処理を中断
+    if (!currentUser || !currentUser.uid) {
+      console.error('User is not authenticated or uid is missing')
+      return
+    }
+
+    // Firestoreにデータを追加
+    await addDoc(collection(db, 'users', currentUser.uid, 'tasks'), {
+      title: taskTitle,
+      is_completed: false,
+      created_at: new Date(),
+    })
+
+    // 入力フィールドをクリア
+    inputRef.current.value = ''
+  }
+
   return (
     <div className='my-8'>
       <h1>FireStoreTest 2</h1>
@@ -48,6 +77,10 @@ export default function FireStoreTest2() {
           </li>
         ))}
       </ul>
+      <div className='py-2'>
+        <input className='border-2 p-2' type='text' ref={inputRef} placeholder='新しいタスクを入力' />
+        <button onClick={handleAddTask}>追加</button>
+      </div>
     </div>
   )
 }
