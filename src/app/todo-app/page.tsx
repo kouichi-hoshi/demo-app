84239ea23fe732f09b5a8c14a19f315eeb952275
kouichi.hoshi <kouichi.hoshi@stella-design.biz>
2024-10-05 23:09:04 +0900

    アプリの説明文をdetails/summaryで開閉するように実装

--- src/app/todo-app/page.tsx
+++ src/app/todo-app/page.tsx
@@ -34,71 +34,71 @@ export default function Home() {
           subTitleClassName={'inline-block rounded bg-neutral-500 px-4 py-1 text-sm text-white'}
         />
         <SortableTaskList />
-        <aside className='border-2 p-8 md:p-16'>
+        <aside className='my-32'>
           <header>
             <h3 className='mb-2 pb-4 text-center text-3xl'>このアプリについて</h3>
           </header>
-          <div className='my-2 text-center'>
-            <button className='button'>開く</button>
-          </div>
-          <section className='my-8'>
-            <h2 className='mb-2 text-2xl'>概要</h2>
-            <p className='mb-4'>
-              アプリ開発の学習のために制作した、シンプルなToDoアプリです。
-              <br />
-              ベータ版として公開していますが、ログインすると、どなたでも無料でお試しいただけます。
-              <br />
-              タスクの追加、編集、削除など一般的なToDoアプリの機能を実装しています。
-            </p>
-            <p className='mb-4'>
-              PC、タブレット、スマートフォンに対応しています。
-              <br />
-              ドラッグ&ドロップでタスクの順番を変更できます。
-              <br />
-              登録したタスクはデータベースに保存され、異なるデバイス間で同期して利用可能です。
-            </p>
-          </section>
-          <section className='my-8'>
-            <h2 className='mb-2 text-2xl'>ログイン方法</h2>
-            <p className='mb-4'>
-              画面右上の「Googleでログイン」ボタンをクリック/タップしてログインすると、ログイン画面が開きます。
-              <br />
-              画面の指示に従ってログインしてください。
-            </p>
-          </section>
-          <section className='my-8'>
-            <h2 className='mb-2 text-2xl'>ログアウトと接続解除</h2>
-            <p className='mb-4'>
-              ログアウトボタンが表示されているときは、そのボタンをクリック/タップするとログアウトできます。
-            </p>
-            <p className='mb-4'>
-              アプリとの接続を解除する場合はGoogleアカウントの
-              <Link href='https://myaccount.google.com/security' target='_blank'>
-                セキュリティページ
-              </Link>
-              で当アプリ（<strong>demo-app</strong>）を解除してください。
-            </p>
-          </section>
-          <section className='my-8'>
-            <h2 className='mb-2 text-2xl'>データの削除について</h2>
-            <p className='mb-4'>
-              アプリと接続を解除しても、登録したタスクの情報はデータベースから削除されません。
-              <br />
-              タスクのデータを完全に削除したい場合は、ログイン中に削除ボタンをクリック/タップして削除してください。
-            </p>
-          </section>
-          <section className='my-8'>
-            <h2 className='mb-2 text-2xl'>開発者について</h2>
-            <p className='mb-4'>
-              このアプリは{masterSiteTitle}が作成しました。
-              <br />
-              開発者のサイトは以下のURLからご覧いただけます。
-              <br />
-              <Link href={masterSiteUrl} target='_blank'>
-                {masterSiteUrl}
-              </Link>
-            </p>
-          </section>
+          <details>
+            <section className='my-12'>
+              <h2 className='mb-2 text-2xl'>概要</h2>
+              <p className='mb-4'>
+                アプリ開発の学習のために制作した、シンプルなToDoアプリです。
+                <br />
+                ベータ版として公開していますが、ログインすると、どなたでも無料でお試しいただけます。
+                <br />
+                タスクの追加、編集、削除など一般的なToDoアプリの機能を実装しています。
+              </p>
+              <ul className='mb-4 ml-4'>
+                <li className='list-disc'>PC、タブレット、スマートフォンに対応しています。</li>
+                <li className='list-disc'>ドラッグ&ドロップでタスクの順番を変更できます。</li>
+                <li className='list-disc'>
+                  登録したタスクはデータベースに保存され、異なるデバイス間で同期して利用可能です。
+                </li>
+              </ul>
+            </section>
+            <section className='my-12'>
+              <h2 className='mb-2 text-2xl'>ログイン方法</h2>
+              <p className='mb-4'>
+                画面右上の「Googleでログイン」ボタンをクリック/タップしてログインすると、ログイン画面が開きます。
+                <br />
+                画面の指示に従ってログインしてください。
+              </p>
+            </section>
+            <section className='my-12'>
+              <h2 className='mb-2 text-2xl'>ログアウトと接続解除</h2>
+              <p className='mb-4'>
+                ログアウトボタンが表示されているときは、そのボタンをクリック/タップするとログアウトできます。
+              </p>
+              <p className='mb-4'>
+                アプリとの接続を解除する場合はGoogleアカウントの
+                <Link href='https://myaccount.google.com/security' target='_blank'>
+                  セキュリティページ
+                </Link>
+                で当アプリ（<strong>demo-app</strong>）を解除してください。
+              </p>
+            </section>
+            <section className='my-12'>
+              <h2 className='mb-2 text-2xl'>データの削除について</h2>
+              <p className='mb-4'>
+                アプリと接続を解除しても、登録したタスクの情報はデータベースから削除されません。
+                <br />
+                タスクのデータを完全に削除したい場合は、ログイン中にタスクの削除ボタンをクリック/タップして削除してください。
+              </p>
+            </section>
+            <section className='my-12'>
+              <h2 className='mb-2 text-2xl'>開発者について</h2>
+              <p className='mb-4'>
+                このアプリは{masterSiteTitle}が作成しました。
+                <br />
+                開発者のサイトは以下のURLからご覧いただけます。
+                <br />
+                <Link href={masterSiteUrl} target='_blank'>
+                  {masterSiteUrl}
+                </Link>
+              </p>
+            </section>
+            <summary className='button mx-auto w-fit hover:cursor-pointer'>このアプリについて</summary>
+          </details>
         </aside>
       </section>
     </article>
