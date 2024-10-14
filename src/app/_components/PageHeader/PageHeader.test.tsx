f943a4fd68c562e6b3ffb4379a877a89925d5f64
kouichi.hoshi <kouichi.hoshi@stella-design.biz>
2024-10-15 07:27:31 +0900

    未使用のimport reactを削除：関数名itを関数名testに変更

--- src/app/_components/PageHeader/PageHeader.test.tsx
+++ src/app/_components/PageHeader/PageHeader.test.tsx
@@ -1,35 +1,34 @@
-import React from 'react'
 import { render, screen } from '@testing-library/react'
 import PageHeader from '../PageHeader'
 
 describe('PageHeader コンポーネント', () => {
-  it('デフォルトのタグ h1 でタイトルが正しくレンダリングされる', () => {
+  test('デフォルトのタグ h1 でタイトルが正しくレンダリングされる', () => {
     render(<PageHeader title='Test Title' />)
     const titleElement = screen.getByRole('heading', { name: /test title/i })
     expect(titleElement).toBeInTheDocument()
     expect(titleElement.tagName).toBe('H1')
   })
 
-  it('指定されたタグでタイトルがレンダリングされる', () => {
+  test('指定されたタグでタイトルがレンダリングされる', () => {
     render(<PageHeader title='Test Title' tag='h3' />)
     const titleElement = screen.getByRole('heading', { name: /test title/i })
     expect(titleElement).toBeInTheDocument()
     expect(titleElement.tagName).toBe('H3')
   })
 
-  it('サブタイトルが提供された場合にレンダリングされる', () => {
+  test('サブタイトルが提供された場合にレンダリングされる', () => {
     render(<PageHeader title='Test Title' subTitle='Test Subtitle' />)
     const subTitleElement = screen.getByText(/test subtitle/i)
     expect(subTitleElement).toBeInTheDocument()
   })
 
-  it('サブタイトルが提供されていない場合にはレンダリングされない', () => {
+  test('サブタイトルが提供されていない場合にはレンダリングされない', () => {
     render(<PageHeader title='Test Title' />)
     const subTitleElement = screen.queryByText(/test subtitle/i)
     expect(subTitleElement).not.toBeInTheDocument()
   })
 
-  it('正しいクラス名が適用される', () => {
+  test('正しいクラス名が適用される', () => {
     render(
       <PageHeader
         title='Test Title'
