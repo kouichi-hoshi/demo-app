30f7425b7b37cc71758b47e41c77dc5f308bb485
kouichi.hoshi <kouichi.hoshi@stella-design.biz>
2024-11-11 21:41:22 +0900

    アイコンを選択不可に指定
    スマートフォン表示のときにアイコンが選択状態になりUXを損なっている

--- src/app/_components/TaskList/index.tsx
+++ src/app/_components/TaskList/index.tsx
@@ -74,7 +74,7 @@ function SortableTaskItem({ task, toggleTaskCompletion, updateTaskTitle, deleteT
       >
         <div className='flex w-full items-center gap-4'>
           {/* この部分がドラッグ専用のハンドル */}
-          <span {...listeners} {...attributes}>
+          <span className='select-none' {...listeners} {...attributes}>
             <FaBars />
           </span>
           <div className='flex w-full flex-col gap-2 md:flex-row md:items-center md:justify-between'>
