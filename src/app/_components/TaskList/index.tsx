42d71fcaf252a0de43745f31ac0d5996ee9bdc9e
kouichi.hoshi <kouichi.hoshi@stella-design.biz>
2024-11-20 22:56:37 +0900

    コメント修正：タスクが完了している場合のみ、削除イベントボタンを表示する仕様に変更：通常表示モードのときにはキャンセルラベルは不要
    通常表示モードのときに、isEditing（真偽値）に応じてキャンセルと編集でボタンのラベルを切り替える仕様で実装していたが、
    この部分のisEditingは常にfalseなので条件分岐を削除した。

--- src/app/_components/TaskList/index.tsx
+++ src/app/_components/TaskList/index.tsx
@@ -78,9 +78,10 @@ function SortableTaskItem({ task, toggleTaskCompletion, updateTaskTitle, deleteT
             <FaBars />
           </span>
           <div className='flex w-full flex-col gap-2 md:flex-row md:items-center md:justify-between'>
-            {/* タスクタイトルの表示モードと編集モードを切り替える */}
+            {/* 編集モード と 通常表示モードの条件分岐 */}
             {isEditing[task.id] ? (
               <>
+                {/* 編集モード */}
                 <div className='grow'>
                   <input
                     ref={titleInputRef}
@@ -114,17 +115,21 @@ function SortableTaskItem({ task, toggleTaskCompletion, updateTaskTitle, deleteT
               </>
             ) : (
               <>
+                {/* 通常表示モード */}
                 <div className='flex items-center gap-2 py-2'>{task.title}</div>
                 <div className='flex shrink-0 justify-end gap-2'>
                   <button className='button-primary' onClick={() => toggleTaskCompletion(task.id, task.is_completed)}>
                     {task.is_completed ? '未完了に戻す' : '完了'}
                   </button>
                   <button className='button-secondary' onClick={() => handleToggleEditMode(task.id)}>
-                    {isEditing[task.id] ? 'キャンセル' : '編集'}
+                    編集
                   </button>
-                  <button className='px-4 py-2 text-xs text-gray-500' onClick={() => deleteTask(task.id)}>
-                    削除
-                  </button>
+                  {/* タスクに完了済みフラグがセットされてるときのみ削除ボタンを表示する */}
+                  {task.is_completed && (
+                    <button className='px-4 py-2 text-xs text-gray-500' onClick={() => deleteTask(task.id)}>
+                      削除
+                    </button>
+                  )}
                 </div>
               </>
             )}
