5c677059ea7e957995951995f1b183a15762f216
kouichi.hoshi <kouichi.hoshi@stella-design.biz>
2024-11-23 17:22:51 +0900

    ドメイン変更に伴い名称とURLを変更

--- src/app/_constants/constants.ts
+++ src/app/_constants/constants.ts
@@ -1,13 +1,13 @@
 export const SITE_META = {
-  siteTitle: 'Demo app - Stella Design',
-  siteUrl: 'https://demo-app.stella-design.biz',
-  siteDesc: 'Demo app for Stella Design',
+  siteTitle: 'Demo app - Stella Dev',
+  siteUrl: 'https://demo-app.stella-dev.biz',
+  siteDesc: 'Demo app for Stella Dev',
   siteLocale: 'ja_JP',
   siteFavicon: '/favicon.ico',
   siteIcon: '/android-chrome-192×192.png',
   siteAppleIcon: '/apple-touch-icon.png',
   openGraphImages: '/sd-img-1200-630.jpg',
-  masterSiteUrl: 'https://stella-design.biz',
-  masterSiteTitle: 'Stella Design',
+  masterSiteUrl: 'https://stella-dev.biz',
+  masterSiteTitle: 'Stella Dev',
 
 }
